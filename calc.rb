a0b00de0aba9799addef6d6cc3b12979468e3146
Jack Henley <jack.d.henley@gmail.com>
2014-01-07 22:03:06 -0500

    Final edits to phase 1 and 2

--- calc.rb
+++ calc.rb
@@ -19,6 +19,7 @@ def answer_prompt(answer)
 	puts "The answer to your problem is: #{answer}"
 end
 
+#define all of the math functions...
 def addition (num1, num2)
 	num1 + num2
 end
@@ -51,53 +52,61 @@ def square_root(num1)
 	Math.sqrt(num1)
 end
 
+def get_it
+	gets.chomp.to_i
+end
+
+
+#use the math type inputted by the user to determine which math function to use
+#All math functions return values, which will be returned by assign_type
 def assign_type(math_type)
 	case math_type
 	when 1
 		puts 'Ok, addition. Give me two numbers to add. First number:'
-		num1 = gets.chomp.to_i
+		num1 = get_it
 		puts 'Second number:'
-		num2 = gets.chomp.to_i
+		num2 = get_it
 		addition(num1, num2)
 	when 2
-		puts 'Ok, subtraction. Give me two numbers to add. First number:'
-		num1 = gets.chomp.to_i
+		puts 'Ok, subtraction. Give me two numbers to subtract. First number:'
+		num1 = get_it
 		puts 'Second number:'
-		num2 = gets.chomp.to_i
+		num2 = get_it
 		subtraction(num1, num2)
 	when 3
-		puts 'Ok, multiplication. Give me two numbers to add. First number:'
-		num1 = gets.chomp.to_i
+		puts 'Ok, multiplication. Give me two numbers to multiply. First number:'
+		num1 = get_it
 		puts 'Second number:'
-		num2 = gets.chomp.to_i
+		num2 = get_it
 		multiplication(num1, num2)
 	when 4
-		puts 'Ok, division. Give me two numbers to add. First number:'
-		num1 = gets.chomp.to_i
+		puts 'Ok, division. Give me two numbers to divide. First number:'
+		num1 = get_it
 		puts 'Second number:'
-		num2 = gets.chomp.to_i
+		num2 = get_it
 		division(num1, num2)
 	when 5
 		puts 'Ok, exponents. Give me a base number:'
-		num1 = gets.chomp.to_i
+		num1 = get_it
 		puts 'And the exponent:'
-		num2 = gets.chomp.to_i
+		num2 = get_it
 		exponent(num1, num2)
 	when 6
 		puts 'Ok, squaring. Give me a number to square:'
-		num1 = gets.chomp.to_i
+		num1 = get_it
 		square(num1)
 	when 7
 		puts 'Ok, cubing. Give me a number to cube:'
-		num1 = gets.chomp.to_i
+		num1 = get_it
 		cube(num1)
 	when 8
 		puts 'Ok, square root. Give me a number to find the root of:'
-		num1 = gets.chomp.to_i
+		num1 = get_it
 		square_root(num1)
 	else
-		puts 'OK, funny guy. Let\'s try that again'
-		input
+		puts 'OK, funny guy. Let\'s try that again.'
+		math_type = input
+		answer = assign_type(math_type)
 	end
 end
 
@@ -107,7 +116,7 @@ end
 #run input function and assign value to variable "math_type"
 math_type = input
 #send number to assign_type, which will require user input and pass variables to other methods
-#assign return value to answer variable
+#assign return value (answer) to answer variable
 answer = assign_type(math_type)
-
+#invoke the answer prompt
 answer_prompt(answer)
